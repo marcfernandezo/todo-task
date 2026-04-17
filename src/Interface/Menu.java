42dc1a0465e6ee918bad25906543bbb9015d98fa
Marc Fernandez <marc.fernaandezo@gmail.com>
2026-04-17 11:41:23 +0200

    chore: initial commit

---
+++ src/Interface/Menu.java
@@ -0,0 +1,39 @@
+package Interface;
+
+import java.util.Scanner;
+
+public class Menu {
+    public static void menu() {
+        Scanner sc = new Scanner(System.in);
+        int option = 0;
+
+        do {
+            System.out.println("==== TODO TASK MANAGER ====");
+
+            System.out.println("Select an option: \n 1. Agregar Tarea \n 2. Editar Tarea" +
+                    "\n 3. Borrar Tarea \n 4. Listar Tarea");
+
+            option = sc.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("-> Has elegido: Agregar Tarea");
+                    break;
+                case 2:
+                    System.out.println("-> Has elegido: Editar Tarea");
+                    break;
+                case 3:
+                    System.out.println("-> Has elegido: Borrar Tarea");
+                    break;
+                case 4:
+                    System.out.println("-> Has elegido: Listar Tareas");
+                    break;
+                case 5:
+                    System.out.println("Saliendo del programa...");
+                    break;
+                default:
+                    System.out.println("Opcion no válida, intenta de nuevo.");
+            }
+        } while (option != 5);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import Interface.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+
+        File file = new File("src/data-tasks.csv");
+        Menu.menu();
+    }
+}
